0c897964ba48c9a665949be8fd532ae11e6ed263
Maja Lichman <maja.lichman@gmx.de>
2026-04-13 20:39:12 +0200

    Update toc-active.js

--- assets/js/toc-active.js
+++ assets/js/toc-active.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const sections = document.querySelectorAll(".content h2");
     const links = document.querySelectorAll("#toc a");
 
+    
     window.addEventListener("scroll", () => {
         let current = "";
 
@@ -15,6 +16,21 @@ document.addEventListener("DOMContentLoaded", function () {
             link.classList.remove("active");
             if (link.getAttribute("href") === "#" + current) {
                 link.classList.add("active");
+            }
+        });
+    });
+
+    links.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href");
+            const target = document.querySelector(targetId);
+
+            if (target) {
+                target.scrollIntoView({ behavior: "smooth" });
+
+                history.replaceState(null, null, targetId);
             }
         });
     });
